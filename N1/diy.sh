d60bb212e7204d700cafa2452fe0935ed3e3a5fd
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2024-08-14 17:49:50 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -5,10 +5,8 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 # Remove packages
 rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb,openvpn}
 
 # Add packages
 git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-git clone https://github.com/kenzok8/small-package package/smpackage
 
