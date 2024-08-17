ae8fdc89debf8efb129291248898cdf9462dad8e
ml-cheng <37533217+ml-cheng@users.noreply.github.com>
2024-08-17 17:31:32 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,9 +4,9 @@
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
+git clone https://github.com/kenzok8/small package/small
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-
